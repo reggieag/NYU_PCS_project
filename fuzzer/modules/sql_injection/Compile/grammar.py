a3e4299147e05ee3a35e4629e815af0ea853dab9
Reggie Gomez <reggieag@gmail.com>
2021-04-17 14:18:32 -0400

    add api module

---
+++ fuzzer/modules/sql_injection/Compile/grammar.py
@@ -0,0 +1,141 @@
+""" THIS IS AN AUTOMATICALLY GENERATED FILE!"""
+from __future__ import print_function
+import json
+from engine import primitives
+from engine.core import requests
+from engine.errors import ResponseParsingException
+from engine import dependencies
+
+_data_post_dataId = dependencies.DynamicVariable("_data_post_dataId")
+
+def parse_datapost(data):
+    """ Automatically generated response parser """
+    # Declare response variables
+    temp_7262 = None
+    # Parse the response into json
+    try:
+        data = json.loads(data)
+    except Exception as error:
+        raise ResponseParsingException("Exception parsing response, data was not valid json: {}".format(error))
+
+    # Try to extract each dynamic object
+
+
+    try:
+        temp_7262 = str(data["dataId"])
+    except Exception as error:
+        # This is not an error, since some properties are not always returned
+        pass
+
+
+    # If no dynamic objects were extracted, throw.
+    if not (temp_7262):
+        raise ResponseParsingException("Error: all of the expected dynamic objects were not present in the response.")
+
+    # Set dynamic variables
+    if temp_7262:
+        dependencies.set_variable("_data_post_dataId", temp_7262)
+
+req_collection = requests.RequestCollection([])
+# Endpoint: /data, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("data"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/data"
+)
+req_collection.add_request(request)
+
+# Endpoint: /data, method: Post
+request = requests.Request([
+    primitives.restler_static_string("POST "),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("data"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_static_string("Content-Type: application/json\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+    primitives.restler_fuzzable_object("{ \"fuzz\": false }"),
+    primitives.restler_static_string("\r\n"),
+    
+    {
+        'post_send':
+        {
+            'parser': parse_datapost,
+            'dependencies':
+            [
+                _data_post_dataId.writer()
+            ]
+        }
+    },
+
+],
+requestId="/data"
+)
+req_collection.add_request(request)
+
+# Endpoint: /data/{dataId}, method: Delete
+request = requests.Request([
+    primitives.restler_static_string("DELETE "),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("data"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string(_data_post_dataId.reader()),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/data/{dataId}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /data/{dataId}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("data"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string(_data_post_dataId.reader()),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/data/{dataId}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /data/{dataId}, method: Post
+request = requests.Request([
+    primitives.restler_static_string("POST "),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("data"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string(_data_post_dataId.reader()),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_static_string("Content-Type: application/json\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+    primitives.restler_fuzzable_object("{ \"fuzz\": false }"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/data/{dataId}"
+)
+req_collection.add_request(request)
