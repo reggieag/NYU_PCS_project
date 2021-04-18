672eec1aea11a9d04482cfba0b67c9814dc0066c
Zhengqi Xi <zhengqixi@gmail.com>
2021-04-17 22:03:45 -0400

    Start parsing securitySchemes section

--- fuzzer/modules/oauth2_scopes/clients.py
+++ fuzzer/modules/oauth2_scopes/clients.py
@@ -1,24 +1,27 @@
 import yaml
 import logging
 
+
 class BadClientConfig(Exception):
     def __init__(self, message='Input clients data is improperly formatted'):
         super().__init__(message)
+
 
 class Client:
     """
     Object to represent to hold data about a client:
     client id, client secret, and client scopes
     """
+
     def __init__(self, id, secret, scopes):
         self._id = id
         self._secret = secret
         self._scopes = scopes
-    
+
     @property
     def id(self):
         return self._id
-    
+
     @property
     def secret(self):
         return self._secret
@@ -27,16 +30,18 @@ class Client:
     def scopes(self):
         return self._scopes
 
+
 class Clients:
     """
     Meta class to manipulate lists of clients, such as creating clients from 
     configuration files, etc
     """
-    def __init__(self, clients_list = None):
+
+    def __init__(self, clients_list=None):
         self._clients = []
         if clients_list is not None:
             self.parse_clients_list(clients_list)
-    
+
     def parse_clients_list(self, clients_list):
         """
         Given a list of client configurations, parses the data
@@ -47,12 +52,12 @@ class Clients:
             raise BadClientConfig
         for client in clients['clients']:
             try:
-                self._clients.append(Client(client['client_id'], client['client_secret'], client['scopes']))
+                self._clients.append(
+                    Client(client['client_id'], client['client_secret'], client['scopes']))
             except KeyError:
-                logging.log(level=logging.WARNING, msg='{} is missing keys. Skipping'.format(client))
-                continue
+                raise BadClientConfig(
+                    message='{} is missing information'.format(client))
 
-    
     @property
     def clients(self):
         return self._clients
--- fuzzer/modules/oauth2_scopes/security.py
+++ fuzzer/modules/oauth2_scopes/security.py
@@ -1,7 +1,10 @@
+import yaml
+
 
 class BadSecuritySchema(Exception):
     def __init__(self, message='Security schema section is improperly formatted'):
         super().__init__(message)
+
 
 class SecurityType:
     """
@@ -10,20 +13,62 @@ class SecurityType:
     This is a base class for actual security types
     that OpenAPI supports
     """
-    pass
+
+    def __init__(self, type):
+        self._type = type
+
+    @property
+    def type(self):
+        return self._type
+
+    def __new__(self, *args, **kwargs):
+        if self is SecurityType:
+            raise TypeError("Cannot instantiate SecurityType base class")
+        return object.__new__(self, *args, **kwargs)
+
 
 class OAuth2SecurityType(SecurityType):
     """
     OAUth2SecurityType represents the Oauth2 security type
     supported by OpenAPI
     """
-    pass
+
+    def __init__(self):
+        super().__init__('oauth2')
+
 
 class OAuth2Flow:
     """
     OAuth2Flow is a base class for various OAuth2 flows
     """
-    pass
+
+    def __init__(self, scopes=[], refresh_url="", token_url="", authorization_url=""):
+        self._scopes = []
+        self._refresh_url = ""
+        self._token_url = ""
+        self._authorization_url = ""
+
+    @property
+    def scopes(self):
+        return self._scopes
+
+    @property
+    def refresh_url(self):
+        return self._refresh_url
+
+    @property
+    def token_url(self):
+        return self._token_url
+
+    @property
+    def authorization_url(self):
+        return self._authorization_url
+
+    def __new__(self, *args, **kwargs):
+        if self is OAuth2Flow:
+            raise TypeError("Cannot instantiate OAuth2Flow base class")
+        return object.__new__(self, *args, **kwargs)
+
 
 class ClientCredentialFlow(OAuth2Flow):
     """
@@ -32,9 +77,28 @@ class ClientCredentialFlow(OAuth2Flow):
     """
     pass
 
-class SecuritySchema:
+
+class SecuritySchemes:
     """
-    SecuritySchema contains information about the types of security
+    SecuritySchemes contains information about the types of security
     an OpenAPI schema provides
     """
-    pass
+
+    def __init__(self):
+        self._schemes = {}
+
+    def parse_schema(self, schema):
+        """
+        Parses the securitySchemes section of an OpenAPI schema
+        """
+        schema = yaml.safe_load(schema)
+        if schema is None:
+            raise BadSecuritySchema
+        try:
+            security_schemes = schema['components']['securitySchemes']
+        except KeyError:
+            raise BadSecuritySchema
+
+    @property
+    def schemes(self):
+        return self._schemes
