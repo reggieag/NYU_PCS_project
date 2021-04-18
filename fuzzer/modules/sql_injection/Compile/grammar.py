c1917d8ba68130d87c1d8b039683979c22e1d0b8
Reggie Gomez <reggieag@gmail.com>
2021-04-17 20:17:26 -0400

    compile new api spec

--- fuzzer/modules/sql_injection/Compile/grammar.py
+++ fuzzer/modules/sql_injection/Compile/grammar.py
@@ -139,3 +139,19 @@ request = requests.Request([
 requestId="/data/{dataId}"
 )
 req_collection.add_request(request)
+
+# Endpoint: /ping, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("ping"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: \r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/ping"
+)
+req_collection.add_request(request)
--- fuzzer/modules/sql_injection/run.py
+++ fuzzer/modules/sql_injection/run.py
@@ -1,16 +1,17 @@
 import time
 import os
+import json
 import sys
 
-print(os.environ)
-
-# TODO: Generate data needed for RESTler
 # TODO: Poll DB to make sure that its up and running
 # TODO: Poll API to make sure that its up and running (In this case, the API will be running the moment the DB is ready)
-# TODO: Call RESTler 
+# TODO: Compile openAPI spec with RESTler
+# TODO: Read in grammar.json
+# TODO: Generate SQL injection calls
 # TODO: Verify DB changes and report error appropriately
 
-time.sleep(4)
-print("pretend we're running")
-time.sleep(4)
-sys.exit("Will this report error back to Go?")
+
+# time.sleep(4)
+# print("pretend we're running")
+# time.sleep(4)
+# sys.exit("Will this report error back to Go?")
