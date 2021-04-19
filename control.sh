ba1024171a049a57f407ef7de9714177c2dccb24
Zhengqi Xi <zhengqixi@gmail.com>
2021-04-19 17:10:21 -0400

    Fix script

--- control.sh
+++ control.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+function ping_api {
+    for i in {1..5..2}
+    do
+        response=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:8080/ping)
+        echo "Ping response: $response"
+        if [ "$response" = "200" ]; then
+            echo "API has started...Ping is responding"
+            exit 0
+        fi
+        sleep $i
+    done
+    echo "API not responding after 3 attempts. Stopping..."
+    exit 1
+}
+
+
 if [ "$1" = "start" ]; then
     docker-compose up --detach
+    ping_api
 elif [ "$1" = "stop" ]; then
     docker-compose down
 else
