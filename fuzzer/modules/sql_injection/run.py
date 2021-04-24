e0fde7745c5b65fc8732aa6fabe8a9b0a92a7862
Reggie Gomez <reggieag@gmail.com>
2021-04-24 10:28:56 -0400

    add a working test against lab6

--- fuzzer/modules/sql_injection/run.py
+++ fuzzer/modules/sql_injection/run.py
@@ -2,7 +2,8 @@ import time
 import os
 import sys
 
-from strategies.blind import blind_sql_injection_test
+from strategies.string_interpolation import string_interpolation_test
+from strategies.lab6_string_interpolation import lab6_string_interpolation_test
 
 # TODO: Pull and construct from environment variables
 HOST_ADDRESS = 'http://127.0.0.1:8080'
@@ -13,14 +14,8 @@ GRAMMAR_FILE = os.path.join(os.path.dirname(__file__), 'Compile/grammar.json')
 
 print(os.environ)
 
-# TODO: Poll DB to make sure that its up and running
-# TODO: Poll API to make sure that its up and running (In this case, the API will be running the moment the DB is ready)
-# TODO: Compile openAPI spec with RESTler
-# TODO: Read in grammar.json
-# TODO: Generate SQL injection calls
-# TODO: Verify DB changes and report error appropriately
-
-blind_sql_injection_test(GRAMMAR_FILE, HOST_ADDRESS)
+string_interpolation_test(GRAMMAR_FILE, HOST_ADDRESS)
+lab6_string_interpolation_test(GRAMMAR_FILE, HOST_ADDRESS)
 
 time.sleep(4)
 print("pretend we're running")
