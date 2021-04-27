8a105571986f50c32a2c6d367137d1a90adb7e57
Reggie Gomez <reggieag@gmail.com>
2021-04-26 20:09:43 -0500

    add more robust logging and bring naming in line with oauth2 module

--- fuzzer/modules/sql_injection/run.py
+++ fuzzer/modules/sql_injection/run.py
@@ -1,6 +1,35 @@
+import os
+import logging
+import sys
+
 from strategies.create_table_strategy import openapi_create_table_attack_test
 
-# TODO: Pull and construct from environment variables
-BASE_URL = 'http://127.0.0.1:8080'
 
-openapi_create_table_attack_test(BASE_URL)
+def run_tests(url):
+    gucci = True
+    gucci = openapi_create_table_attack_test(url) and gucci
+    if gucci:
+        sys.exit(0)
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    schema = os.getenv('API_SCHEMA')
+    # TODO: Pull and construct from environment variables
+    url = 'http://127.0.0.1:8080'
+    # url = os.getenv('API_URL')
+
+    log_level = os.getenv('LOG_LEVEL')
+    logging_format = '%(levelname)s:module_oauth2_scopes:%(message)s'
+    if log_level == 'DEBUG':
+        logging.basicConfig(format=logging_format, level=logging.DEBUG)
+    elif log_level == 'INFO':
+        logging.basicConfig(format=logging_format, level=logging.INFO)
+    elif log_level == 'WARNING':
+        logging.basicConfig(format=logging_format, level=logging.WARNING)
+    elif log_level == 'ERROR':
+        logging.basicConfig(format=logging_format, level=logging.ERROR)
+    else:
+        logging.basicConfig(format=logging_format, level=logging.INFO)
+
+    run_tests(url)
--- fuzzer/modules/sql_injection/strategies/create_table_strategy.py
+++ fuzzer/modules/sql_injection/strategies/create_table_strategy.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 import uuid
 
@@ -6,32 +7,36 @@ from functools import partial
 from .lib.postgres import get_tables, drop_table, DBCONFIG
 
 
-def openapi_api_call_generator(base_url, uuid):
+def openapi_api_call_generator(base_url, attack_param):
     # TODO: Implement this for reals
-    attack = f"' create table {uuid} ; -- "
-    data = {'name': attack, 'quantity': 2}
+    data = {'name': attack_param, 'quantity': 2}
     return [('http://127.0.0.1:8080/data/1', data, 'post')]
 
 
 def create_table_attack_test(base_url, api_call_generator):
+    gucci = True
     attack_uuid = uuid.uuid4()
-    for url, data, method in api_call_generator(base_url, attack_uuid):
-        print(f"testing {url}")
+    attack = f"' where 1=2 ; create table {attack_uuid} (id int); -- "
+    for url, body, method in api_call_generator(base_url, attack):
+        logging.info(f"Testing generated {url} method {method} body {body}")
         if method == 'post':
-            requests.post(url, data=data)
+            requests.post(url, data=body)
         elif method == 'get':
-            requests.get(url, data=data)
+            requests.get(url, data=body)
         elif method == 'delete':
-            requests.delete(url, data=data)
+            requests.delete(url, data=body)
         elif method == 'patch':
-            requests.patch(url, data=data)
+            requests.patch(url, data=body)
 
         tables = get_tables(DBCONFIG)
 
-        if any([True for table in tables if attack_uuid in table]):
-            print(f"Vulnerability found for {url} {data} {method}")
+        if any([True for table in tables if str(attack_uuid) == table[0]]):
+            logging.warning(f"Vulnerability found for {url} method {method} body {body}")
+            gucci = False
 
         drop_table(DBCONFIG, attack_uuid)
+
+    return gucci
 
 
 openapi_create_table_attack_test = partial(create_table_attack_test, api_call_generator=openapi_api_call_generator)
